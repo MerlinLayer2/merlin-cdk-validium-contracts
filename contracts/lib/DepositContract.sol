4b39836d3a19d90b291b6d9eb46753600ff4d502
Surou <cppfuns@gmail.com>
2024-01-23 16:38:32 +0800

    fix# audit

--- contracts/lib/DepositContract.sol
+++ contracts/lib/DepositContract.sol
@@ -65,27 +65,21 @@ contract DepositContract is ReentrancyGuardUpgradeable {
     function _deposit(bytes32 leafHash) internal {
         bytes32 node = leafHash;
 
-        // Avoid overflowing the Merkle tree (and prevent edge case in computing `_branch`)
         if (depositCount >= _MAX_DEPOSIT_COUNT) {
             revert MerkleTreeFull();
         }
 
-        // Add deposit data root to Merkle tree (update a single `_branch` node)
-        uint256 size = ++depositCount;
-        for (
-            uint256 height = 0;
-            height < _DEPOSIT_CONTRACT_TREE_DEPTH;
-            height++
-        ) {
+        // Update Merkle tree branch
+        uint256 size = depositCount;
+        for (uint256 height = 0; height < _DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
             if (((size >> height) & 1) == 1) {
                 _branch[height] = node;
-                return;
+                break;
             }
             node = keccak256(abi.encodePacked(_branch[height], node));
         }
-        // As the loop should always end prematurely with the `return` statement,
-        // this code should be unreachable. We assert `false` just to be safe.
-        assert(false);
+
+        depositCount++;
     }
 
     /**
